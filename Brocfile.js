c5dddf54368e76f280f5e1c17a73d8682b43ead6
Max Schoening <mail@maximilian.io>
2015-06-09 16:27:07 -0700

    Add auto-prefixing
    This only auto-prefixes when running the addon server. Still figuring out how to properly register a pre-processor.

--- Brocfile.js
+++ Brocfile.js
@@ -11,6 +11,11 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
   behave. You most likely want to be modifying `./index.js` or app's Brocfile
 */
 
-var app = new EmberAddon();
+var app = new EmberAddon({
+  autoprefixer: {
+    browsers: ['last 2 ios version'],
+    cascade: false
+  }
+});
 
 module.exports = app.toTree();
